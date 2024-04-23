dce37903ec438736a8d3bd42fb5f34968390f2a0
Jackson Smith <i-s-j-o@proton.me>
2024-04-23 07:47:41 -0400

    working on stuff idk

--- c/fig06_08.c
+++
@@ -1,8 +0,0 @@
-// Treating character arrays as strings.
-#include <stdio.h>
-#define SIZE 20
-
-int main(void) {
-  char string1[SIZE] = "";
-  char string2[] = "string literal"; // Reserves 15 characters.
-}
--- python/foo.py
+++ python/foo.py
@@ -3,7 +3,18 @@
 
 
 def get_name():
-    """Main function begins program execution."""
-    # Get name from user.
     name = input("Please enter your name: ")
     return name
+
+
+def print_name(name):
+    """Prints user name."""
+    print(name)
+
+
+def main():
+    """Print name."""
+    print("Hello, " + name + "!")
+
+
+main()
